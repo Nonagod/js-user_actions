be7767a7c41504b238bd54d7dd51f3d88e9925df
Nonagod <danilov.ss-pr@yandex.ru>
2024-06-24 18:34:04 +0300

    build: донастроена система сборки проекта

---
+++ esbuild.config.mjs
@@ -0,0 +1,18 @@
+import esbuild from 'esbuild';
+import babel from 'esbuild-plugin-babel';
+
+// console.log( process.env.NODE_ENV );
+
+esbuild.build({
+    entryPoints: ['src/index.web.js'],
+    bundle: true,
+    minify: true,
+    sourcemap: true,
+    plugins: [babel()], // берет настройки из .babelrc, preset-env учитывает target из .browserslistrc
+    target: ['chrome58', 'firefox57', 'safari11', 'edge16'],
+    outfile: 'dist/PROJECT_NAME.min.js',
+}).catch(() => process.exit(1));
+
+// Пришлось добавлять конфигурацию и babel т.к. esbuild давай ошибку на деструкторе (es5 синтаксис)
+// esbuild src/index.web.js --bundle --minify --sourcemap --target=chrome58,firefox57,safari11,edge16 --outfile=dist/PROJECT_NAME.min.js
+// Transforming destructuring to the configured target environment is not supported yet
