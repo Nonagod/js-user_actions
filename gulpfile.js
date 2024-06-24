228a77b593ef02dc0cc43faf6c11e3f981e61dd6
Nonagod <danilov.ss-pr@yandex.ru>
2024-06-24 13:49:01 +0300

    build: добавлена новая система сборки и конфигурации проекта

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
-let {
-    TARGET_FILE_NAME,
-    TARGET_FOLDER_PATH,
-    SOURCE_FILE_PATH
-} = require( './_resources/gulp/settings.js' ),
-gulp = require('gulp'),
-webpack = require('webpack-stream');
+const TARGET_FILE_NAME = 'libs.min.js';
+const TARGET_FOLDER_PATH = 'build';
+
+const SOURCE_FILE_PATH = 'src/index.js';
+
+let gulp = require('gulp'),
+    webpack = require('webpack-stream');
 
 gulp.task('default', function() {
     return gulp.src( SOURCE_FILE_PATH )
